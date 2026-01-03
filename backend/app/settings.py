cd544d551c762ea63453fbf8558958e0f491317d
Rubi <rubi.marin.ponce@gmail.com>
2026-01-03 17:46:11 +0100

    refactor: replace polls app with exercices app and update related configurations; add initial models, views, and migrations for exercices

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
+    "exercices.apps.ExercicesConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ backend/exercices/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100) # ex: Sécurité informatique
+    icon = models.CharField(max_length=10)   # ex: 🛡️
+    slug = models.SlugField(unique=True)     # ex: securite
+    order = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Section(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sections')
+    name = models.CharField(max_length=100) # ex: Chiffrement César
+    slug = models.SlugField()
+    order = models.IntegerField(default=0)
+    
+    # Pour la partie théorie
+    theory_content = models.TextField(blank=True) # Texte en Markdown
+    python_example = models.TextField(blank=True) # Bloc de code python
+
+    def __str__(self):
+        return f"{self.category.name} - {self.name}"
+
+class ExerciseAttempt(models.Model):
+    # Utile si vous voulez des stats (même sans compte élève, pour l'admin)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    success = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
